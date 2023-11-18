a4ecdc8699993b0efd05434c0e0f2b9054d6dfe4
Keane Quigley <keaneq@protonmail.com>
2023-11-18 23:57:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RADIANT
+All code written for my master's dissertation titled _Multilevel Radial Basis Function Approximation of PDEs_.
+
+# References
+[1] Wendland, H. (2004). _Scattered Data Approximation_ (Cambridge Monographs on Applied and Computational Mathematics). Cambridge: Cambridge University Press. [doi:10.1017/CBO9780511617539](doi:10.1017/CBO9780511617539)
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='radiant',
+    version='0.0.0',
+    packages=[
+        'radiate',
+    ],
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    maintainer='J. Keane Quigley',
+    maintainer_email='james.quigley@st-hildas.ox.ac.uk',
+    description='Multilevel Radial Basis Function Approximation of PDEs.',
+    license='MIT',
+    python_requires=">=3.8",
+)
