fix phi derivative

radiant/integrate/trapezoid.py
@@ -1,13 +1,12 @@
 from .base import BaseIntegrator
-import cupy as cp
-
+import numpy as np
 
 class TrapezoidIntegrator(BaseIntegrator):
     def __init__(self, a, b, accuracy):
         super().__init__(a, b, accuracy)
 
     def __call__(self, func):
-        xs = cp.linspace(self.a, self.b, self.accuracy * int(self.b - self.a))
+        xs = np.linspace(self.a, self.b, self.accuracy * int(self.b - self.a))
         ys = func(xs)
 
-        return cp.trapz(ys, xs)
+        return np.trapz(ys, xs)

radiant/solve/base.py
@@ -1,5 +1,5 @@
 import cupy as cp
-from numpy.linalg import cond
+import numpy as np
 
 
 class BaseSolver:
@@ -22,10 +22,10 @@ class BaseSolver:
 
         self.gen_rhs(func, guess)
 
-        weights = cp.linalg.solve(self.mat, cp.array(self.b))
+        weights = cp.linalg.solve(cp.array(self.mat), cp.array(self.b)).get()
 
         def approximant(x, *, m=0):
-            return cp.sum(
+            return np.sum(
                 self.phi(x, self.centres, self.delta, weights, m=m),
                 axis=0,
             )
@@ -36,4 +36,4 @@ class BaseSolver:
         if self.mat is None:
             return -1.
         else:
-            return cond(self.mat.get())
+            return np.linalg.cond(self.mat)

radiant/solve/helmholtz.py
@@ -1,5 +1,6 @@
 from .base import BaseSolver
-import cupy as cp
+import numpy as np
+
 
 def _lhs_integrand_factory(xi, xj, delta, phi):
     def func(x):
@@ -30,7 +31,7 @@ class HelmholtzBaseSolver(BaseSolver):
         self.integrator = integrator
 
     def gen_mat(self):
-        self.mat = cp.zeros((self.centres.shape[0], self.centres.shape[0]))
+        self.mat = np.zeros((self.centres.shape[0], self.centres.shape[0]))
 
         for i, xi in enumerate(self.centres):
             self.mat[i, i] = self.integrator(
@@ -45,7 +46,7 @@ class HelmholtzBaseSolver(BaseSolver):
                 self.mat[j, i] = self.mat[i, j]
 
     def gen_rhs(self, func, guess):
-        self.b = cp.zeros_like(self.centres)
+        self.b = np.zeros_like(self.centres)
         for i, xi in enumerate(self.centres):
             self.b[i] = self.integrator(
                 _rhs_integrand_factory(func, xi, self.delta, self.phi, guess)

radiant/solve/multilevel.py
@@ -1,4 +1,4 @@
-import cupy as cp
+import numpy as np
 
 
 class MultilevelFunction(list):
@@ -9,7 +9,7 @@ class MultilevelFunction(list):
         if end is None:
             end = len(self)
 
-        val = cp.zeros_like(x)
+        val = np.zeros_like(x)
         for s in self[:end]:
             val += s(x, *args, **kwargs)
 

radiant/util.py
@@ -1,8 +1,8 @@
-import cupy as cp
+import numpy as np
 
 
 def error(u, approx, integrator):
     numerator = integrator(lambda x: (u(x) - approx(x)) ** 2)
     denominator = integrator(lambda x: u(x) ** 2)
 
-    return cp.sqrt(numerator / denominator)
+    return np.sqrt(numerator / denominator)

radiant/visualise/plot.py
@@ -1,4 +1,4 @@
-import cupy as cp
+import numpy as np
 import matplotlib.pylab as plt
 
 
@@ -10,10 +10,10 @@ def many(a, b, *funcs, n=1000, labels=None, **kwargs):
 
     plt.figure(**kwargs)
     plt.margins(x=0.)
-    xs = cp.linspace(a, b, n * int(b - a))
+    xs = np.linspace(a, b, n * int(b - a))
 
     for func, label in zip(funcs, labels):
-        plt.plot(xs.get(), func(xs).get(), label=label)
+        plt.plot(xs, func(xs), label=label)
 
     if leg:
         plt.legend()
@@ -23,7 +23,7 @@ def many(a, b, *funcs, n=1000, labels=None, **kwargs):
 def thinning(centres, delta, **kwargs):
     plt.figure(**kwargs)
     for c, d in zip(centres, delta):
-        plt.plot(c.get(), (d * cp.ones_like(c)).get(), 'b.')
+        plt.plot(c, (d * np.ones_like(c)), 'b.')
 
     plt.yscale('log')
     plt.show()

radiant/wendland.py
@@ -1,5 +1,4 @@
 from math import comb
-import cupy as cp
 import numpy as np
 
 
@@ -49,6 +48,14 @@ class Wendland:
                 "Smoothness 'k' must be one of 0, 1, 2, or 3."
             )
 
+        if d >= 2:
+            def norm(x):
+                return np.linalg.norm(x, axis=0)
+
+            self.norm = norm
+        else:
+            self.norm = np.abs
+
     def __call__(self, x, c, delta, w=None, m=0):
         if w is None:
             w = np.ones_like(c)
@@ -56,10 +63,12 @@ class Wendland:
         if w.shape != ():
             w = w[:, None]
 
-        r = np.abs(np.subtract.outer(c, x))
+        diff = - np.subtract.outer(c, x)
+        normed_diff = np.abs(diff)
+        r = normed_diff / delta
         unsupported = np.multiply(
             w,
-            self.poly.deriv(m)(r / delta) / (delta ** m),
+            (diff / (delta * normed_diff + 1e-6)) ** m * self.poly.deriv(m)(r),
         )
 
-        return cp.array(np.where(1 - r / delta >= 0, unsupported, 0))
+        return np.where(1 - r >= 0, unsupported, 0)