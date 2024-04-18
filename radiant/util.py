8d0ee63ec3087e5c9c323e0d610c9a1d3b4d5d3f
Keane Quigley <keaneq@protonmail.com>
2024-04-18 15:46:55 +0100

    Plots for report

--- radiant/util.py
+++ radiant/util.py
@@ -8,9 +8,8 @@ def dot(a, b):
 
 def error(u, approx, integrator):
     numerator = integrator(lambda *x: (u(*x) - approx(*x)) ** 2)
-    denominator = integrator(lambda *x: u(*x) ** 2)
 
-    return np.sqrt(numerator / denominator)
+    return np.sqrt(numerator)
 
 
 def flatten(x):
